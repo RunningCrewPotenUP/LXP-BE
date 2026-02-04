52c1916599506142695b5fa810ba6ad10c011144
betterb2etr <b2etr@naver.com>
2026-02-04 16:39:35 +0900

    init main

---
+++ src/main/java/com/lxpbe/LxpBeApplication.java
@@ -0,0 +1,13 @@
+package com.lxpbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxpBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxpBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxpbe/LxpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxpbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxpBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
