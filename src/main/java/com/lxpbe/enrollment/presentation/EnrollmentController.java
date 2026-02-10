5305eaabae31cd19d7bdc7eda13e007b718d7dee
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-10 14:52:20 +0900

    refactor(enrollment): 수강 등록 api 파라미터 validation 추가

--- src/main/java/com/lxpbe/enrollment/presentation/EnrollmentController.java
+++ src/main/java/com/lxpbe/enrollment/presentation/EnrollmentController.java
@@ -3,6 +3,8 @@ package com.lxpbe.enrollment.presentation;
 import com.lxpbe.enrollment.application.EnrollmentCommandService;
 import com.lxpbe.enrollment.application.EnrollmentQueryService;
 import com.lxpbe.enrollment.presentation.response.EnrollmentCreatedResponse;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +29,13 @@ public class EnrollmentController {
 
     @PostMapping
     public ResponseEntity<EnrollmentCreatedResponse> enroll(
-            @LoginUser Long userId,
-            @RequestParam Long courseId
+            @LoginUser
+            Long userId,
+
+            @RequestParam
+            @NotNull(message = "courseId 는 필수입니다.")
+            @Positive(message = "courseId 는 음수일 수 없습니다.")
+            Long courseId
     ) {
         EnrollmentCreatedResponse response = enrollmentCommandService.enroll(userId, courseId);
         return ResponseEntity
