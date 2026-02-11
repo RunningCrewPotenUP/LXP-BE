050336a441f0f1c717342c8746615c9e6d871a94
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-11 23:16:36 +0900

    fix(tag): 태그 api base url 수정

--- src/main/java/com/lxpbe/tag/presentation/TagController.java
+++ src/main/java/com/lxpbe/tag/presentation/TagController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Validated
 @RestController
-@RequestMapping("/api-v1/tags")
+@RequestMapping("/tags")
 public class TagController {
 
     private final TagQueryService tagQueryService;
