4b0c567b7dcb076ebc3716c52dbb44fb32c739ad
dd-jiyun <dlwldbs8015@gmail.com>
2026-02-11 23:53:26 +0900

    feat(swagger): Auth Api swagger 설정

--- src/main/java/com/lxpbe/auth/presentation/controller/AuthController.java
+++ src/main/java/com/lxpbe/auth/presentation/controller/AuthController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthApi {
     private final AuthService authService;
     private final CookieProvider cookieProvider;
 
