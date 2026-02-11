392bd2322e666fd131badc282840786ff85f1d12
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-12 00:13:13 +0900

    feat(enrollment): 수강 조회용 리포지토리 구현

--- src/main/java/com/lxpbe/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/lxpbe/enrollment/repository/EnrollmentRepository.java
@@ -1,10 +1,13 @@
 package com.lxpbe.enrollment.repository;
 
 import com.lxpbe.enrollment.domain.model.Enrollment;
+import com.lxpbe.enrollment.repository.view.EnrollmentDetailsView;
+import com.lxpbe.enrollment.repository.view.EnrollmentSummaryView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +15,51 @@ import java.util.Optional;
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     Optional<Enrollment> findByUserIdAndCourseIdAndCancelledAtIsNull(Long userId, Long courseId);
 
-    List<Enrollment> findAllByUserId(Long userId);
+    @Query("""
+    SELECT e.id as enrollmentId,
+            e.enrollmentStatus as enrollmentStatus,
+            e.enrolledAt as enrolledAt,
+            e.learningStartedAt as learningStartedAt,
+            e.cancelledAt as cancelledAt,
+            e.cancelType as cancelType,
+            e.cancelReasonType as cancelReasonType,
+            e.cancelReasonComment as cancelReasonComment,
+            u.id as instructorId,
+            u.name as instructorName,
+            c.id as courseId,
+            c.thumbnailUrl as thumbnailUrl,
+            c.title as courseTitle,
+            c.description as courseDescription,
+            c.difficulty as courseLevel,
+            c.tags as tags
+    FROM Enrollment e
+    JOIN Course c ON c.id = e.courseId
+    JOIN User u ON u.id = c.instructorId
+    WHERE e.id = :enrollmentId
+    """)
+    EnrollmentDetailsView projectEnrollmentDetails(@Param("enrollmentId") Long enrollmentId);
+
+    @Query("""
+    SELECT e.id as enrollmentId,
+            e.enrollmentStatus as enrollmentStatus,
+            e.enrolledAt as enrolledAt,
+            e.learningStartedAt as learningStartedAt,
+            e.cancelledAt as cancelledAt,
+            e.cancelType as cancelType,
+            e.cancelReasonType as cancelReasonType,
+            e.cancelReasonComment as cancelReasonComment,
+            u.id as instructorId,
+            u.name as instructorName,
+            c.id as courseId,
+            c.thumbnailUrl as thumbnailUrl,
+            c.title as courseTitle,
+            c.description as courseDescription,
+            c.difficulty as courseLevel,
+            c.tags as tags
+    FROM Enrollment e
+    JOIN Course c ON c.id = e.courseId
+    JOIN User u ON u.id = c.instructorId
+    WHERE e.userId = :userId
+    """)
+    List<EnrollmentSummaryView> projectEnrollmentSummaries(@Param("userId") Long userId);
 }
