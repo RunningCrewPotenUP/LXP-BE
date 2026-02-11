d9f16769cd62c81aefb17224993d895ba3fe2a75
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-12 00:13:13 +0900

    feat(enrollment): 수강 조회용 리포지토리 구현

---
+++ src/main/java/com/lxpbe/enrollment/repository/view/EnrollmentSummaryView.java
@@ -0,0 +1,35 @@
+package com.lxpbe.enrollment.repository.view;
+
+import com.lxpbe.enrollment.domain.model.enums.CancelReasonType;
+import com.lxpbe.enrollment.domain.model.enums.CancelType;
+import com.lxpbe.enrollment.domain.model.enums.EnrollmentStatus;
+import com.lxpbe.tag.domain.Tag;
+
+import java.time.Instant;
+import java.util.List;
+
+public interface EnrollmentSummaryView {
+
+    Long getEnrollmentId();
+    EnrollmentStatus getEnrollmentStatus();
+    Instant getEnrolledAt();
+    Instant getLearningStartedAt();
+    Instant getCancelledAt();
+    CancelType getCancelType();
+    CancelReasonType getCancelReasonType();
+    String getCancelReasonComment();
+
+    Long getInstructorId();
+    String getInstructorName();
+
+    Long getCourseId();
+    String getThumbnailUrl();
+    String getCourseTitle();
+    String getCourseDescription();
+    String getCourseLevel();
+
+    List<Tag> getTags();
+
+    // To Do: 진행도 구현 후 반영
+    // Double getTotalProgress();
+}
