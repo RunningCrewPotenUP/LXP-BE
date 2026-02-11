931089b4250c0f440dcbab95fa58a133ef8c13fe
dd-jiyun <dlwldbs8015@gmail.com>
2026-02-11 12:24:43 +0900

    feat(user) : 유저 조회 facade 로직 구현

---
+++ src/main/java/com/lxpbe/user/application/service/UserQueryService.java
@@ -0,0 +1,39 @@
+package com.lxpbe.user.application.service;
+
+import com.lxpbe.tag.domain.Tag;
+import com.lxpbe.tag.repository.TagRepository;
+import com.lxpbe.user.domain.User;
+import com.lxpbe.user.infrastructure.facade.dto.UserProfileDto;
+import com.lxpbe.user.infrastructure.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserQueryService {
+    private final UserRepository userRepository;
+    private final TagRepository tagRepository;
+
+    public Optional<UserProfileDto> findById(Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> {
+                    List<String> tagNames = getTagNames(user);
+                    return new UserProfileDto(
+                            user.getId(),
+                            tagNames,
+                            user.getLevel());
+                });
+    }
+
+    private List<String> getTagNames(User user) {
+        return tagRepository.findAllByIdIn(user.getTagIds())
+                .stream()
+                .map(Tag::name)
+                .toList();
+    }
+
+}
