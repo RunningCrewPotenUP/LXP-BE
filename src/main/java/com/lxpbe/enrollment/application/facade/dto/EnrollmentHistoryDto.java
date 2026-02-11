cd9c1b3d99b3f7681685a7699dcbc155f6a83c4b
SeungYeop-Han <ammezkhan@gmail.com>
2026-02-11 13:53:43 +0900

    feat(enrollment): 빌더 및 팩토리 메서드 정의

--- src/main/java/com/lxpbe/enrollment/application/facade/dto/EnrollmentHistoryDto.java
+++ src/main/java/com/lxpbe/enrollment/application/facade/dto/EnrollmentHistoryDto.java
@@ -1,7 +1,11 @@
 package com.lxpbe.enrollment.application.facade.dto;
 
+import com.lxpbe.enrollment.domain.model.Enrollment;
+import lombok.Builder;
+
 import java.util.Objects;
 
+@Builder
 public record EnrollmentHistoryDto(
         Long enrollmentId,
         Long userId,
@@ -14,5 +18,14 @@ public record EnrollmentHistoryDto(
         Objects.requireNonNull(enrollmentId, REQUIRED_FIELD_ERROR_MESSAGE + "userId");
         Objects.requireNonNull(enrollmentId, REQUIRED_FIELD_ERROR_MESSAGE + "courseId");
         Objects.requireNonNull(enrollmentId, REQUIRED_FIELD_ERROR_MESSAGE + "status");
+    }
+
+    public EnrollmentHistoryDto of(Enrollment enrollment) {
+        return EnrollmentHistoryDto.builder()
+                .enrollmentId(enrollment.id())
+                .userId(enrollment.userId())
+                .courseId(enrollment.courseId())
+                .status(enrollment.enrollmentStatus().name())
+                .build();
     }
 }
